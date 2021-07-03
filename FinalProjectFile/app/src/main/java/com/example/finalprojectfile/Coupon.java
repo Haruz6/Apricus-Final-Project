61ec7d6c4667d27a78a8ebf3d56638cc5ce9b038
SuPorong <1250726362@qq.com>
2021-07-03 21:01:01 +0800

    Adding Quiz and Coupon Activity.

---
+++ FinalProjectFile/app/src/main/java/com/example/finalprojectfile/Coupon.java
@@ -0,0 +1,14 @@
+package com.example.finalprojectfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Coupon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coupon);
+    }
+}
